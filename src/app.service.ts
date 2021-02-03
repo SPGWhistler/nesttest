91c6f72d0a770f3c3c5b6c74cd46e61c60f74763
Tony Petty <tpetty@synacor.com>
2021-02-03 10:37:54 -0500

    testing

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+const Keyv = require('keyv');
+const KeyvFile = require('keyv-file').KeyvFile;
+
+const keyv = new Keyv({
+  store: new KeyvFile({
+    filename: './titles.json'
+  })
+});
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<any> {
+    return await keyv.get('000');
   }
 }
