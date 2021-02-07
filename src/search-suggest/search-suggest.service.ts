a6c39f233d1dd574a8fa7f57f1b38b34611afe6e
Tony Petty <tpetty@synacor.com>
2021-02-06 19:21:11 -0500

    search suggestions are working

--- src/search-suggest/search-suggest.service.ts
+++ src/search-suggest/search-suggest.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import got from 'got';
+import { parse } from 'fast-xml-parser';
 const Keyv = require('keyv');
 const KeyvFile = require('keyv-file').KeyvFile;
 
@@ -11,6 +13,43 @@ const keyv = new Keyv({
 @Injectable()
 export class SearchSuggestService {
   async getTitlesForQuery(query: string): Promise<any> {
-    return await keyv.get(query);
+    let results;
+    try {
+      results = await keyv.get(query);
+    } catch (ignored) {}
+    if (!Array.isArray(results)) {
+      results = await this.makeRequest(query);
+      results = this.cleanResults(results);
+      await keyv.set(query, results);
+    }
+    return results;
+  }
+  async makeRequest(query: string): Promise<Array<any>> {
+    const resp = await got(
+      `https://www.goodreads.com/search/index.xml?key=RDfV4oPehM6jNhxfNQzzQ&page=1&search=all&q=${query}`,
+    );
+    const jsonObj = parse(resp.body, {});
+    let results = [];
+    if (
+      jsonObj &&
+      jsonObj.GoodreadsResponse &&
+      jsonObj.GoodreadsResponse.search &&
+      jsonObj.GoodreadsResponse.search.results &&
+      jsonObj.GoodreadsResponse.search.results.work
+    ) {
+      if (!Array.isArray(jsonObj.GoodreadsResponse.search.results.work)) {
+        jsonObj.GoodreadsResponse.search.results.work = [
+          jsonObj.GoodreadsResponse.search.results.work
+        ];
+      }
+      results = jsonObj.GoodreadsResponse.search.results.work;
+    }
+    return results;
+  }
+
+  cleanResults(results: Array<any>): Array<any> {
+    results = results.map((result) => result.best_book.title);
+    results = results.slice(0, 5);
+    return results; //TODO
   }
 }
