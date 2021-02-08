189a1bcbde1dbbba8c3f85bc87f9201a00ec5f45
Tony Petty <tpetty@synacor.com>
2021-02-07 22:07:22 -0500

    working

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { SearchService } from './search.service'
+import { SearchService, Results } from './search.service'
 
 @Controller('/v1/search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
   @Get()
-  async getSearchResults(@Query('q') query: string): Promise<Array<any>> {
-    return await this.searchService.getResultsForQuery(query);
+  async getSearchResults(
+    @Query('q') query: string,
+    @Query('page') page = '1',
+  ): Promise<Results> {
+    return await this.searchService.getResultsForQuery(
+      query,
+      parseInt(page, 10),
+    );
   }
 }
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -3,6 +3,11 @@ import { Cache } from 'cache-manager';
 import got from 'got';
 import { parse } from 'fast-xml-parser';
 
+export interface Results {
+  totalPages: number;
+  results: Array<any>;
+}
+
 @Injectable()
 export class SearchService {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
@@ -13,7 +18,7 @@ export class SearchService {
    * - Add in documentation and also swager docs
    * - add in pagination
    */
-  async getResultsForQuery(query: string): Promise<Array<any>> {
+  async getResultsForQuery(query: string, page = 1): Promise<Results> {
     /**
      * - Check cache for query.
      * - Make request to GoodReads for results.
@@ -21,26 +26,30 @@ export class SearchService {
      * - Cache results.
      * - Return results.
      */
-    let results;
-    try {
-      results = await this.cacheManager.get('searchquery:' + query);
-    } catch (ignored) {}
-    if (!Array.isArray(results)) {
-      results = await this.makeRequest(query);
+    let results: Results;
+    if (page === 1) {
+      try {
+        results = await this.cacheManager.get('searchquery:' + query);
+      } catch (ignored) {}
+    }
+    if (!results || !Array.isArray(results.results)) {
+      results = await this.makeRequest(query, page);
       results = this.cleanResults(results);
-      await this.cacheManager.set('searchquery:' + query, results, {
-        ttl: 86400,
-      }); //cache for 24 hours
+      if (page === 1) {
+        await this.cacheManager.set('searchquery:' + query, results, {
+          ttl: 86400,
+        }); //cache for 24 hours
+      }
     }
     return results;
   }
 
-  async makeRequest(query: string): Promise<Array<any>> {
+  async makeRequest(query: string, page = 1): Promise<Results> {
     const resp = await got(
-      `https://www.goodreads.com/search/index.xml?key=RDfV4oPehM6jNhxfNQzzQ&page=1&search=all&q=${query}`,
+      `https://www.goodreads.com/search/index.xml?key=RDfV4oPehM6jNhxfNQzzQ&page=${page}&search=all&q=${query}`,
     );
     const jsonObj = parse(resp.body, {});
-    let results = [];
+    let results: Results;
     if (
       jsonObj &&
       jsonObj.GoodreadsResponse &&
@@ -53,13 +62,30 @@ export class SearchService {
           jsonObj.GoodreadsResponse.search.results.work
         ];
       }
-      results = jsonObj.GoodreadsResponse.search.results.work;
+      results = {
+        results: jsonObj.GoodreadsResponse.search.results.work,
+        totalPages: this.calculateTotalPages(
+          jsonObj.GoodreadsResponse.search['total-results'] || 0,
+        ),
+      };
     }
     return results;
   }
 
-  cleanResults(results: Array<any>): Array<any> {
-    results = results.map((result) => {
+  calculateTotalPages(totalResults: number): number {
+    if (totalResults <= 0) {
+      return 0; //no results
+    } else if (totalResults > 0 && totalResults < 20) {
+      return 1; //1 page of results
+    } else if (totalResults / 20 > 100) {
+      return 100; //more than 100 pages, api only returns 100 tho, so limit it to that
+    } else {
+      return Math.ceil(totalResults / 20); //return actual number of pages
+    }
+  }
+
+  cleanResults(results: Results): Results {
+    results.results = results.results.map((result) => {
       return {
         id: result.best_book.id,
         title: result.best_book.title,
